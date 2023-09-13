7c11fe678da27b79b55c067c3b526e5e318f7bca
Gabriel <caampos22@gmail.com>
2023-09-12 21:17:24 -0300

    feat ✨: Add transcriptions generation

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,21 @@
-import { fastify } from 'fastify'
-
-import { getAllPromptsRoute } from './routes/get-all-prompts'
-import { uploadVideoRoute } from './routes/upload-video'
+import { fastifyCors } from '@fastify/cors';
+import { fastify } from "fastify";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
 
 const app = fastify()
 
+app.register(fastifyCors, {
+  origin: '*'
+})
+
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
 
 app.listen({
-  port: 3333,
+  port: 3333
 }).then(() => {
   console.log('HTTP Server Running!')
 })
