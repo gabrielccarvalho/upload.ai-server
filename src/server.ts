caa62f6a6762926607bc0923a4bf4808820f9002
Gabriel <caampos22@gmail.com>
2023-09-12 21:30:41 -0300

    feat ✨: Add AI completion route

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { fastifyCors } from '@fastify/cors';
 import { fastify } from "fastify";
 import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletionRoute } from './routes/generate-ai-completion';
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 
@@ -13,6 +14,7 @@ app.register(fastifyCors, {
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
 
 app.listen({
   port: 3333
