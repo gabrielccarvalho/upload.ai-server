19703a150b548620ab34d2a212da2ac279f526fa
Gabriel <caampos22@gmail.com>
2023-09-13 21:32:48 -0300

    feat ✨: Add stream return to AI completion

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -1,9 +1,8 @@
 import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
+import { streamToResponse, OpenAIStream } from 'ai'
 import { openai } from '../lib/openai';
 import { prisma } from '../lib/prisma';
-
-
 
 export async function generateAICompletionRoute(app: FastifyInstance) {
   app.post('/ai/complete', async (req, reply) => {
@@ -32,9 +31,17 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
       temperature,
       messages: [
         { role: 'user', content: promptMessage }
-      ]
+      ],
+      stream: true,
     })
 
-    return response
+    const stream = OpenAIStream(response)
+
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+      }
+    })
   })
 }
